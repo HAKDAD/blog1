ff765546314fdc1037721ea434a2f85c841fb538
Mr_Kali_Hacker <kottevamsi38@gmail.com>
2023-06-16 12:26:35 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,17 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+apiKey: "AIzaSyD4hyVtGMhWW9VVKZIKRHHiv78c0912cqA",
+
+  authDomain: "blog-bb051.firebaseapp.com",
+
+  projectId: "blog-bb051",
+
+  storageBucket: "blog-bb051.appspot.com",
+
+  messagingSenderId: "710370523516",
+
+  appId: "1:710370523516:web:a08a3ff051c68f91488cf9",
+
+  measurementId: "G-GN93HMXEG3"
 };
 
 firebase.initializeApp(firebaseConfig);
